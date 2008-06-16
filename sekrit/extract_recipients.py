7a3d1695d5e26ee6742e7a122a76b9ac41fa616a
Tommi Virtanen <tv@eagain.net>
2008-06-16 19:33:33 +0300

    Add code to extract recipients from encrypted messages.

---
+++ sekrit/extract_recipients.py
@@ -0,0 +1,45 @@
+from __future__ import with_statement
+
+import sys
+import GnuPGInterface
+
+def extract_recipients(path):
+    # See gpg FAQ, /usr/share/doc/gnupg2/faq.html question 4.12, "How
+    # can I get list of key IDs used to encrypt a message?". Note that
+    # the result is often subkeys, not primary keys.
+    gpg = GnuPGInterface.GnuPG()
+
+    with file(path, 'rb') as f:
+        fpr = gpg.run(
+            [
+                '--decrypt',
+                '--list-only',
+                ],
+            create_fhs=[
+                'status',
+                # have to redirect stdout as nose sets stdout to be a
+                # cStringIO, which has no fileno method
+                'stdout',
+                ],
+            attach_fhs=dict(
+                stdin=f,
+                ),
+            )
+
+        recipients = []
+        for line in fpr.handles['status']:
+            PREFIX = '[GNUPG:] ENC_TO '
+            if not line.startswith(PREFIX):
+                continue
+            keyid = line[len(PREFIX):].split(None, 1)[0]
+
+            yield keyid
+
+        for line in fpr.handles['stdout']:
+            print >>sys.stderr, 'GnuPG stdout:', line.rstrip('\n')
+
+        try:
+            fpr.wait()
+        except IOError, e:
+            print >>sys.stderr, '%s: gnupg: %s' % (sys.argv[0], e)
+            sys.exit(1)
---
+++ sekrit/test/test_extract_recipients.py
@@ -0,0 +1,58 @@
+from __future__ import with_statement
+
+import os
+
+from nose.tools import (
+    eq_ as eq,
+    )
+
+from sekrit.test.util import (
+    assert_raises,
+    maketemp,
+    )
+
+from sekrit import (
+    extract_recipients,
+    )
+
+def test_simple():
+    data = """
+-----BEGIN PGP MESSAGE-----
+Version: GnuPG v1.4.6 (GNU/Linux)
+
+hQIOA/JiGU+GKWbNEAf+Mj23UceAZbB0F1aiM7/3JJdoEqKqof1L/zdPU3YF1STX
+dcGVrERNgLgxk8hheLQUSrTOZ9THhQuFmkWkBkB6NKOn5pElxAOWmB04496Q+Fux
+xXiNA/CHdlgRzrYj22WErmuUgbv8mIMBDrEcIhj775d2aXiBr864AN+LpP6l/3XR
+t0jbIdf6JbswN0e/Vwgw6i1JWSl62vYHpuD9fOzoVrCdiwNAVBAun1bYmZAYAA/F
+1fGIwfNaD4z3AXzapkYcctu3KBn9sd2NewRicDTdHTEXBrN3HNfkjU0HAHaP8vGI
+hNNnFGwEcbm/CWfU8Q6wXozZQty/6jhYcop0yZJzcgf/dtmQ9fAdnfVraSggbM1v
+yEwZq6OHGgBHHWrET6YQ/QG7MgZOEHQSBemaD6qvB4QLc/8nxhZrZG6GOXDz5QbO
+1HFDEcmlwN3gWPypMCJlJ+rHdWkaU9Bap8JuULGUZZ+VUeQI0MBWYwaeweIL5/5+
+xT8XPa+Ez74EInxbNwEGzLNpjy6m8zaLFaWHu52cS7sRaYrhv1Nj1oW6XcVNhYAY
+d/yMrNLqUfpKF0pqb4WVV1b1tqi4igNP9Cl9GWs2QBSbiabFsJZjwoFT7IhXlZJl
+79R+zV37OUM5RhR0KuHvfvqKka3hyiOyi/pNOPnUeZ+0WxDILWIZHEF3LS4a7+sN
+8oUCDgPwUTneWoUOhRAH/3O5H0+Pby76M6I/i0OdmGmrSHbUl++M/+XCLX8qVfDe
+AKf9n8tzsWb1tO7uVs1NivsGmWSToZ9o45fQd9k5pwwr/tE3oyCbtFgkmtKLlPrP
+6dMXUjlF8sRk3KCGUPAOa69/Y0USSQ5M6HVj9TFeS9lrUOnyy570wX6nuwM1lKuB
+4t8zg/LnZgUSfHXdUmJJGUjAP0ppsmrWKnqTtdtIMbUhwKhKGWXBHXvpII/u97ED
+BJ3hFEDs0gjeSk1OU79pzQ8fz2/ZXifm8IYYyNQ3a0eA0EjRNaGF3Xgr9jI+Pwku
+cTQZV1cw0YTajXgXHxA/N6ddi035psLjaqTrfq2Sk2UH+gNpfJdvI8YTwCWsf01b
+ACoWxNf7GyNrLUjjo6P2kSG27H1IvyBdkL48ghFw2vR3/k6B059I27CLbKDn20as
+e6BgJnMea8aqIhbLbPQtfW2OnlfWCyOx2HTUa2F+zGpqWADX9r6J8hnQVjsBD1FG
+0TGmCHIqe+BCsBXERdFU3fJh1JjZg5rvdo+gpZ4CAuOupBfrH0f5h7pcQb8oKhLP
+wxgYpMGYsElS2ioyam/EqO45fMh1p+tJ/kGPb5sL20ORJc63knW1olI04VEzjY87
+esl3/iBHiZ+YKKasJum3KD4gfkglW955Igj+9pUNI73hvoN83yXuhIbOPicZg3Hh
+zHTSPwHRiwIVoo8ULFoRdUxgdL9zUdVz/+7R+pyaIQ4RVvQz0QFXH3rjDBow0N1n
+QXbPbHR4+golsi6vouzOp7VIrQ==
+=0e7b
+-----END PGP MESSAGE-----
+"""
+    tmp = maketemp()
+    path = os.path.join(tmp, 'message')
+    with file(path, 'w') as f:
+        f.write(data)
+
+    g = extract_recipients.extract_recipients(path=path)
+    eq(g.next(), 'F262194F862966CD')
+    eq(g.next(), 'F05139DE5A850E85')
+    assert_raises(StopIteration, g.next)
